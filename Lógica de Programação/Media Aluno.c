ae51b4015b84cee4d980a647c828c195f07a8212
Daniel Correia <130627373+Danielprogrammer01@users.noreply.github.com>
2023-09-27 15:14:52 -0300

    Add files via upload

---
+++ Lógica de Programação/Media Aluno.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main() 
+
+{
+setlocale(LC_ALL,"portuguese");
+
+// Declarando as variáveis.
+
+float primeiraNota, segundaNota, media;
+
+// Solicitando dados ao usuário.
+
+printf("Digite a primeira nota: ");
+scanf("%f", &primeiraNota);
+
+printf("Digite a segunda nota: ");
+scanf("%f", &segundaNota);
+
+media = (primeiraNota + segundaNota) / 2;
+
+// Exibindo dados para o usuário.
+
+printf("1ª Nota: %.1f \n", primeiraNota);
+printf("2ª Nota: %.1f \n", segundaNota);
+printf("Média: %.1f", media);
+
+    return 0;
+}
