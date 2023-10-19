d89cafc9258cb49854d75dd713bba69a4fc138a7
Daniel Correia <130627373+Danielprogrammer01@users.noreply.github.com>
2023-10-19 16:52:07 -0300

    Add files via upload

---
+++ Lógica de Programação/Exercício 26.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include <time.h>
+
+int main()
+{
+
+    setlocale(LC_ALL, "portuguese");
+
+
+    return 0;
+
+}
